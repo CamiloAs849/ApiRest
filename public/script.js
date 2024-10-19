3b92b3b497ed563c40aee1284068c3097235e4ab
CamiloAs849 <osoriojuancamilo315@gmail.com>
2024-10-19 10:18:25 -0500

    TERMINADO

---
+++ public/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch(`http://localhost:3000/empleados`)
+    .then((response) => response.json())
+    .then((empleados) => {
+      const tbody = document.getElementById("empleados-tbody");
+      empleados.forEach((empleado) => {
+        const row = document.createElement("tr");
+
+        const idCell = document.createElement("td");
+        idCell.textContent = empleado.documento;
+        row.appendChild(idCell);
+
+        const nombreCell = document.createElement("td");
+        nombreCell.textContent = empleado.nombre;
+        row.appendChild(nombreCell);
+
+        const botonCell = document.createElement("td");
+        botonCell.innerHTML = `<button type="button" class="btn btn-secondary" onclick="cargarDatos(${empleado.id})">Ver más</button>`;
+        row.appendChild(botonCell);
+
+        tbody.appendChild(row);
+      });
+    })
+    .catch((error) => console.error("Error al obtener los empleados:", error));
+});
+
+function cargarDatos(id) {
+  fetch(`http://localhost:3000/empleados/${id}`)
+    .then((response) => response.json())
+    .then((empleado) => {
+      document.getElementById("id").value = empleado.documento;
+      document.getElementById("nombre").value = empleado.nombre;
+      document.getElementById("puesto").value = empleado.puesto;
+      document.getElementById("salario").value = empleado.salario;
+    });
+}
+
+console.log(document.getElementById("cantidad").value);
+
+function insertarEmpleados(cantidad) {
+  fetch(`http://localhost:3000/generarEmpleados`)
+    .then((response) => response.json())
+    .then((data) => {});
+
+  window.location.reload();
+}
---
+++ servidor.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const mysql = require("mysql2");
+const path = require("path");
+const { faker } = require("@faker-js/faker");
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "empleados",
+  password: "",
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta para obtener todos los empleados
+app.get("/empleados", (req, res) => {
+  connection.query(
+    "SELECT * FROM Empleados ORDER BY id DESC",
+    (err, results) => {
+      if (err) {
+        return res.status(500).send("Error al recuperar los empleados");
+      }
+      res.json(results);
+    }
+  );
+});
+
+app.get("/empleados/:id", (req, res) => {
+  const id = req.params.id;
+  connection.query(
+    "SELECT * FROM Empleados WHERE ID = ?",
+    [id],
+    (err, results) => {
+      if (err) {
+        return res.status(500).send("Error al recuperar el empleado");
+      }
+      if (results.length === 0) {
+        return res.status(404).send("Empleado no encontrado");
+      }
+      res.json(results[0]);
+    }
+  );
+});
+
+app.get("/generarEmpleados", (req, res) => {
+  const empleados = [];
+
+  // Generar 10 empleados ficticios
+  for (let i = 0; i < 10; i++) {
+    const nuevoEmpleado = {
+      id: faker.number.int((min = 5000000), (max = 2147483647)),
+      Nombre: faker.name.fullName(),
+      Puesto: faker.name.jobTitle(),
+      Salario: faker.finance.amount(1000, 6000, 0),
+    };
+
+    // Insertar cada empleado generado en la base de datos
+    connection.query(
+      "INSERT INTO Empleados (documento, nombre, puesto, salario) VALUES (?, ?, ?, ?)",
+      [
+        nuevoEmpleado.id,
+        nuevoEmpleado.Nombre,
+        nuevoEmpleado.Puesto,
+        nuevoEmpleado.Salario,
+      ],
+      (err, results) => {
+        if (err) {
+          return res.status(500).send("Error al insertar empleado");
+        }
+        // Responder con todos los empleados una vez que se hayan insertado
+        if (empleados.length === 10) {
+          res.status(201).json("success");
+        }
+      }
+    );
+  }
+});
+app.listen(3000, () => {
+  console.log(`Servidor corriendo en http://localhost:3000`);
+});
